3da4fcd3b1d54114983d0fb53b298d0b39b3768d
Bipul Lamsal <bipullamsal@gmail.com>
2024-06-05 14:34:54 +0545

    refactor: redfined regex

--- utils/extractor.py
+++ utils/extractor.py
@@ -1,20 +1,34 @@
 import re
-
 def extract_functions(content):
-    # regex based on c++ syntex 
-    function_pattern = re.compile(r'''
-    ([\w\s*&:]+)      # Return type
-        \s+(\w+::\w+|\w+) # Function name
-        \s*\(([^\)]*)\)   # Parameters
-        \s*({(?:[^{}]*{[^{}]*})*[^{}]*})  # Function body
+    # Regular expression to match function signatures without bodies
+    function_signature_pattern = re.compile(r'''
+        ([\w\s*&:<>]+)      # Return type (including templates)
+        \s+(\w+::\w+|\w+)   # Function name
+        \s*\(([^)]*)\)      # Parameters
+        \s*{                # Opening brace of function body
     ''', re.VERBOSE)
 
-    functions = function_pattern.findall(content)
+    matches = function_signature_pattern.finditer(content)
     extracted_functions = []
 
-    for func in functions:
-        return_type, name, parameters, body = func
+    for match in matches:
+        return_type, name, parameters = match.groups()
+        start = match.end() - 1
+        body, end = extract_body(content, start)
         combined_function = f"{return_type.strip()} {name.strip()}({parameters.strip()}) {body.strip()}"
-        extracted_functions.append((name,combined_function))
+        extracted_functions.append((name, combined_function))
     
-    return extracted_functions;  
+    return extracted_functions
+
+def extract_body(content, start):
+    stack = []
+    end = start
+    for i in range(start, len(content)):
+        if content[i] == '{':
+            stack.append('{')
+        elif content[i] == '}':
+            stack.pop()
+            if not stack:
+                end = i + 1
+                break
+    return content[start:end], end
